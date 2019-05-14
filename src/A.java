169c9e2bef5a956ee00be4a0deed915200061cb7
熊猫 <1131585494@qq.com>
2019-05-15 00:52:16 +0800

    idea

--- src/A.java
+++ src/A.java
@@ -1,4 +1,32 @@
+import java.util.Objects;
+
 public class A {
     //测试
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        A a = (A) o;
+        return Objects.equals(name, a.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public A(String name) {
+        this.name = name;
+    }
 }
 
